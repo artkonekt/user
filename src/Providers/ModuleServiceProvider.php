7db3d6c07be3d6fff9d8c13d6d852d3bb0bc6e4f
Attila Fulop <fulopattila122@gmail.com>
2016-11-30 14:25:56 +0200

    Added concord module basic files

---
+++ src/Providers/ModuleServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Contains the ModuleServiceProvider class.
+ *
+ * @copyright   Copyright (c) 2016 Attila Fulop
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2016-11-30
+ *
+ */
+
+
+namespace Konekt\User\Providers;
+
+
+use Konekt\Concord\AbstractModuleServiceProvider;
+
+class ModuleServiceProvider extends AbstractModuleServiceProvider
+{
+
+}
---
+++ src/resources/manifest.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'    => 'Konekt User Module',
+    'version' => '0.1.0'
+];
