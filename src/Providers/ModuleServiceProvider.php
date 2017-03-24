b8615709cd005bf9b31fef1dd23b4fb4492b6d96
Attila Fulop <fulopattila122@gmail.com>
2017-03-24 19:04:51 +0200

    Added initial binding of local user model class to the interface

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -14,8 +14,17 @@ namespace Konekt\User\Providers;
 
 
 use Konekt\Concord\AbstractModuleServiceProvider;
+use Konekt\User\Contracts\UserInterface;
+use Konekt\User\Models\Entities\User;
 
 class ModuleServiceProvider extends AbstractModuleServiceProvider
 {
+    public function register()
+    {
+        parent::register();
+
+        $this->app->concord->useModel(UserInterface::class, User::class);
+    }
+
 
 }
